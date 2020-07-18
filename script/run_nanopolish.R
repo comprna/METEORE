bfeb57483872d42f7eee08722742d9c70fa4228c
zakayuen <zaka0621@gmail.com>
2020-07-19 01:15:48 +1000

    Add Snakemake pipieline for Nanopolish

---
+++ script/run_nanopolish.R
@@ -0,0 +1,66 @@
+#!/usr/bin/env Rscript
+
+# Author: Zaka Yuen, JCSMR, ANU
+# Created on May 2020
+
+# This script is to:
+# -calculate per-site methylation frequency (no. of methylated reads/total reads) from the log-likelihood ratio
+# -use the cutoff of 2.5 suggested by Nanopolish to remove non-significant methylation call
+# 	-log-likelihood ratio >=2.5, considered as methylated 
+# 	-log-likelihood ratio <=-2.5, considered as unmethylated
+
+library(dplyr)
+library(ggplot2)
+library(forcats)
+library(plyr)
+library(data.table)
+library(reshape2)
+
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "out.txt"
+}
+
+df <- read.table(args[1], header=TRUE, sep = "\t")
+df <- select(df, -Read_ID)
+df$Chr <- as.character(levels(df$Chr))[df$Chr]
+df$Strand <- as.character(levels(df$Strand))[df$Strand]
+# Apply the cutoff
+df <- filter(df, Log.like.ratio >= 2.5 | Log.like.ratio <= -2.5)
+# Correct the coordinates
+df[df$Strand == "-", "Pos"] <- df[df$Strand == "-", "Pos"]+1
+
+# The position for same site on differnt stands repeats themselves, need to count no. of methylated CpG and unmethylated CpG using dlyr package
+df_m <- count(df[df$Log.like.ratio > 0, ], c("Chr", "Pos","Strand"))
+df_unm <- count(df[df$Log.like.ratio < 0, ], c("Chr", "Pos","Strand"))
+colnames(df_m)[4] <- "Num.methylated"
+colnames(df_unm)[4] <- "Num.unmethylated"
+# New df
+df <- full_join(df_m, df_unm, by = c("Chr", "Pos", "Strand"))
+# Replace NA with 0
+df[is.na(df)] <- 0
+# Create a new column that gives a total no. of reads (coverage)
+df$df.cov <- df$Num.methylated + df$Num.unmethylated
+# Get the methylation frequency by (no. of methylated reads / total no. of reads)
+df$df.freq <- df$Num.methylated/df$df.cov
+# Remove unnessary columns
+df <- select(df, Chr, Pos, Strand, df.cov, df.freq)
+
+# Accumulate CpG sites into +'ve strand
+df[df$Strand == "-","Pos"] <-
+  df[df$Strand == "-","Pos"]-1
+
+# Use data.table to compute the mean of duplicated position while keeping non-duplicated sites
+df <- data.table(df)
+#str(df)
+df <- df[,list(df.freq = mean(df.freq),
+                df.cov = sum(df.cov)),
+                list(Chr,Pos)]
+df <- data.frame(df)
+
+write.table(df, file=args[2],  quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
---
+++ script/split_cpg_groups.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+import re
+
+"""
+	split_cpg_groups.py
+	Author: Zaka Yuen, JCSMR, ANU
+	Created on Feb 18 2020
+
+	The output of the nanopolish calling procedure is a log-likelihood ratio, 
+	where a positive log-likelihood ratio indicates evidence for methylation.
+	Nanopolish groups nearby CpG sites together and calls the group jointly,
+	assigning the same scores to each site in the group.
+
+	This script is to:
+	-use after running nanopolish call-methylation
+	-allow per-site comparison to other datasets
+	-split up the CpG group into its constituent CpG sites and assign the same log-likelihood ratio
+	-keeping strandedness and readID information
+"""
+
+ch=[]
+st=[]
+pos=[]
+log=[]
+readID=[]
+target ="CG"
+
+with open(snakemake.input[0],'r') as fh:
+    next(fh)
+    for line in fh:
+        fields = line.rstrip().split()
+        chrom = str(fields[0])
+        strand = str(fields[1])
+        start = int(fields[2]) + 1 # fix the position
+        read_name = str(fields[4])
+        logRatio = float(fields[5])
+        cpg_num = int(fields[9])
+        seq = str(fields[10])
+
+        if cpg_num == 1:
+            ch.append(chrom)
+            st.append(strand)
+            pos.append(start)
+            log.append(logRatio)
+            readID.append(read_name)
+
+        elif cpg_num > 1:
+            index = []
+            pos.append(start)
+            for match in  re.finditer(target, seq):
+                #print(match.start())
+                index.append(match.start())
+                length = len(index)
+            for i in range(1, length):
+                new_start = start + (index[i] - index[0])
+                pos.append(new_start)
+                #print(new_start)
+            ch.extend([chrom] * cpg_num)
+            st.extend([strand] * cpg_num)
+            log.extend([logRatio] * cpg_num)
+            readID.extend([read_name] * cpg_num)
+
+final = pd.DataFrame(list(zip(ch,pos,st,log,readID)), columns =['Chr', 'Pos','Strand', 'Log.like.ratio', 'Read_ID'])
+final.to_csv(snakemake.output[0], sep="\t", header=True, index=False)
+
+
